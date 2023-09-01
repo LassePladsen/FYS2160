d5338c786c30434f6a3522565d9d4fed2168f032
Rogumann <lasse.pladsen@hotmail.com>
2023-09-01 12:16:08 +0200

    finetuning of plot

--- Obligs/Oblig 1/plot_data.py
+++ Obligs/Oblig 1/plot_data.py
@@ -10,9 +10,21 @@ time = data[:, 0] / 60
 temp1 = data[:, 1]
 temp2 = data[:, 2]
 
+plt.figure(figsize=(7, 4))
 plt.plot(time, temp1, time, temp2)
 plt.legend(["Bodum thermos", "Temperfect thermos"])
 plt.xlabel("Time [min]")
 plt.ylabel("Water temperature $T_w$ [°C]")
-plt.grid()
+
+# Grid ticks
+xmin = 0
+xmax = 116
+plt.xticks(range(xmin, xmax, 10))
+plt.xticks(range(xmin, xmax, 5), minor=True)
+ymin = 20
+ymax = 91
+plt.yticks(range(ymin, ymax, 10))
+plt.yticks(range(ymin, ymax, 5), minor=True)
+plt.grid(which="both")
+
 plt.savefig("figures/termokopper_plot.png")
