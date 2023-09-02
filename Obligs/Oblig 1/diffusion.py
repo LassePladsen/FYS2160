e46b5925a4fc2aa6e2ff114a0ee1bc496c2c9512
Rogumann <lasse.pladsen@hotmail.com>
2023-09-02 12:09:43 +0200

    added metalblocks experimental data

--- Obligs/Oblig 1/diffusion.py
+++ Obligs/Oblig 1/diffusion.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Parameters
-Ctb = 2  # Heat capacity ratio top to bottom
+Ctb = 1  # Heat capacity ratio top to bottom
 N = 8000  # Number of heat packets
 nstep = 15 * N  # Number of steps in simulation
 tau = N  # Time scale (characteristic time)
@@ -30,9 +30,32 @@ for i in range(1, nstep):
 
 # Plot
 plt.figure()
+plt.subplot(2, 1, 1)
 plt.plot(np.arange(0, nstep)/N, Tt, color='r')
 plt.plot(np.arange(0, nstep)/N, Tb, color='k')
 plt.xlabel('steps/N')
 plt.ylabel(r'$2(T-<T_0>)/\Delta T_0$')
 plt.legend(['Temperature top', 'Temperature bottom'])
+
+
+D = np.loadtxt('metalblocks_lecture.txt', usecols = [0, 1, 2], unpack = True)
+# Guessing the characteristic time
+tau = 100
+# Dimensionless time
+t = D[0,:] / tau
+T1 = D[1,:]
+T2 = D[2,:]
+T10 = np.mean(T1[70:91])
+T20 = np.mean(T2[70:91])
+DT0 = T20 - T10
+Tmean0 = (T20 + T10) / 2
+
+# Plot rescaled temperature versus rescaled time
+plt.subplot(2, 1, 2)
+plt.plot(t, 2 * (T1 - Tmean0) / DT0, color = 'k')
+plt.plot(t, 2 * (T2 - Tmean0) / DT0, color = 'r')
+plt.axis([0, 15, -1.0, 1.01])
+plt.xlabel(r'$t/\tau$')
+plt.ylabel(r'$2(T-<T_0>)/\Delta T_0$')
+plt.legend(['Temperature top','Temperature bottom'])
 plt.savefig("figures/diffusion_simulation.png")
