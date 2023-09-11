af931e3f66309b6c8aad58fa6d20d9d5fe1e6c51
Rogumann <lasse.pladsen@hotmail.com>
2023-09-11 12:12:46 +0200

    2.3

--- Weekly exercises/36/2.3.py
+++ Weekly exercises/36/2.3.py
@@ -8,4 +8,7 @@ import matplotlib.pyplot as plt
 def mult(N_opp, N):
     return np.math.factorial(N)/(np.math.factorial(N_opp) * np.math.factorial(N-N_opp))
 
-print(mult(25,50))
+def prob(N_opp ,N):
+    return mult(N_opp, N)/(2**50)
+
+print(prob(25))
